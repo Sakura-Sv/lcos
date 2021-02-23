afc8d49ea838a327644df7c901eb59e5f00ee584
LiuZhen <1204736871@qq.com>
2021-02-23 23:25:42 +0800

    编写中断处理模块，编写断点异常处理函数

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,15 +1,26 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use lazy_static;
 
-static IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+lazy_static::lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
-    idt.load();
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: &mut InterruptStackFrame
 ) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    x86_64::instructions::interrupts::int3();
 }
