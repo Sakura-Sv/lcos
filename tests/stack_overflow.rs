daa512bf3c4bb9d15a89bbadd39aa0713ff5fa92
LiuZhen <1204736871@qq.com>
2021-03-01 22:11:10 +0800

    完成stack_overflow测试

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,56 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use lcos::{serial_print, serial_println, exit_qemu, QemuExitCode};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use lazy_static;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    lcos::gdt::init();
+    init_test_idt();
+
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    volatile::Volatile::new(0).read();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    lcos::test_panic_handler(info)
+}
+
+lazy_static::lazy_static!{
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+            .set_handler_fn(test_double_fault_handler)
+            .set_stack_index(lcos::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: &mut InterruptStackFrame,
+    _error_code: u64
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
