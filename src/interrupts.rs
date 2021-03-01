5c41a9a833493812aabf57cdd8e14711da550923
LiuZhen <1204736871@qq.com>
2021-03-01 22:45:37 +0800

    引入8259中断控制器

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,6 +2,14 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 use crate::gdt;
 use lazy_static;
+use pic8259_simple::ChainedPics;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe {
+        ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static::lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -30,6 +38,7 @@ extern "x86-interrupt" fn double_fault_handler(
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
+
 
 #[test_case]
 fn test_breakpoint_exception() {
